475068ff56695cb0dc72fb9bc7533652a2b4b9ac
Jiri Lojda <lojdaj@seznam.cz>
2025-05-22 19:11:52 +0200

    Remove kc-source header

--- src/clients/kontentClients.ts
+++ src/clients/kontentClients.ts
@@ -1,11 +1,4 @@
 import { createManagementClient } from '@kontent-ai/management-sdk';
-import { readFileSync } from 'node:fs';
-import { join } from 'node:path';
-
-const packageJsonPath = join(import.meta.dirname, '../../package.json');
-const packageJson = JSON.parse(readFileSync(packageJsonPath, 'utf8'));
-
-const sourceTrackingHeaderName = 'X-KC-SOURCE';
 
 /**
  * Creates a Kontent.ai Management API client
@@ -20,10 +13,6 @@ export const createMapiClient = (
   return createManagementClient({
     apiKey: apiKey ?? process.env.KONTENT_API_KEY ?? throwError("KONTENT_API_KEY is not set"),
     environmentId: environmentId ?? process.env.KONTENT_ENVIRONMENT_ID ?? throwError("KONTENT_ENVIRONMENT_ID is not set"),
-    headers: [{
-      header: sourceTrackingHeaderName,
-      value: `${packageJson.name};${packageJson.version}`,
-    }],
   });
 };
 
